c3389beccc87af62b7d3e53be8caa3b991a453bb
biancaconroy <biconroy@calpoly.edu>
2025-07-15 12:28:49 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,3 +1,4 @@
-print("Hello")
+print("Hello, welcome to ClubTime?")
 # This is a simple Python script that prints "Hello" to the console.
-print("Rise and shine!")
+city=input("What city are you looking for?")
+print("Ok, we will find you clubs in {city}")
